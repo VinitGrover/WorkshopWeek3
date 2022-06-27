9c022d0ebc0c91190dff1476e422e062e67f7f7c
vinitgrover <vinitgrover255@gmail.com>
2022-06-27 19:37:08 +0530

    Node File Appending Data

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,7 +4,7 @@ public class LinkedListMain {
     public static void main(String[] args) {
 
         LinkedList<Integer> myList = new LinkedList<>();
-        myList.append(48);
+        myList.append(48); 
         myList.append(32);
         myList.append(44);
 
